5c3bebcc5267d0c972652c080bd829f05b155dc4
codecnomad <100487345+CodecNomad@users.noreply.github.com>
2026-05-09 14:21:59 +0200

    Added --url CLI command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use anyhow::{Context, Result};
+use clap::Parser;
 use inquire::Text;
 use viuer::Config;
 
+#[derive(Parser)]
+#[command(name = "terminal-image")]
+#[command(version = "1.0")]
+#[command(about = "Draws an image from a http/s link into your terminal", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    url: Option<String>,
+}
+
+fn parse_cli() -> Result<String> {
+    let parsed_cli = Cli::parse();
+
+    parsed_cli.url.map_or_else(
+        || {
+            Text::new("What's the URL of the image?")
+                .prompt()
+                .context("Failed to ask image URL")
+        },
+        Ok,
+    )
+}
+
 fn main() -> Result<()> {
-    let url = Text::new("What's the url of the image?")
-        .prompt()
-        .context("Failed to ask image URL")?;
+    let url = parse_cli()?;
 
     let image_data = reqwest::blocking::get(url)
         .context("Failed to get image from URL")?
